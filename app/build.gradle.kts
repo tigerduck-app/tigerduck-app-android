01015bbd1768bf6546423a29a51678a8e38082e8
SamWang8891 <g348.8891@smashit.tw>
2026-04-07 01:07:58 +0800

    feat: update Gradle and plugin versions, remove unused Kotlin plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
@@ -35,11 +34,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-
-    buildFeatures {
+buildFeatures {
         compose = true
     }
 }
