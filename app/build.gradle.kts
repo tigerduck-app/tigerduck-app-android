707a755278e9ffddf45c471d15e784a48e8b38af
SamWang8891 <g348.8891@smashit.tw>
2026-05-04 16:28:50 +0800

    chore(release): bump version to 1.3.2 and update F-Droid configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "org.ntust.app.tigerduck"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.3.1"
+        versionCode = 11
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
